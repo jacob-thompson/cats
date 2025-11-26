53c523fddc80bc220efc5f02eeeb9dfa1af67376
Jacob Thompson <jacobalthompson@gmail.com>
2025-11-26 05:45:17 -0800

    fix cat.py

--- cat.py
+++ cat.py
@@ -1,24 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
-import shutil
 
-def usage():
-    sys.stderr.write("Usage: cat [file]\n")
-    sys.exit(1)
-
-if len(sys.argv) > 2:
-    usage()
-
-if len(sys.argv) == 2:
+def main():
     try:
-        with open(sys.argv[1], 'rb') as f:
-            shutil.copyfileobj(f, sys.stdout.buffer)
-    except OSError as e:
-        sys.stderr.write(f"{sys.argv[1]}: {e.strerror}\n")
-        sys.exit(1)
-else:
-    try:
-        shutil.copyfileobj(sys.stdin.buffer, sys.stdout.buffer)
-    except OSError:
-        sys.exit(1)
+        for line in sys.stdin:
+            sys.stdout.write(line)
+    except KeyboardInterrupt:
+        exit(130)
+
+if __name__ == "__main__":
+    main()
